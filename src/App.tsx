f749ea3450efec112129be5c07be4822a0aeb5fe
Muhammad Ibrahim <the.ibrahim.sultani@gmail.com>
2023-09-29 02:48:13 +0500

    mui5 added along with absolue imports

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,30 @@ import { theme } from 'utils/mui';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = ()=> {
+const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
           </a>
-          <Button variant='contained'>Contained Button</Button>
-          <Button variant='outlined'>Outlined Button</Button>
-          <Checkbox/>
-      </header>
+          <Button variant="contained">Contained Button</Button>
+          <Button variant="outlined">Outlined Button</Button>
+          <Checkbox />
+        </header>
       </div>
-      </ThemeProvider>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
--- src/utils/mui/index.ts
+++ src/utils/mui/index.ts
@@ -2,7 +2,7 @@ import { createTheme } from '@mui/material';
 import { green, purple } from '@mui/material/colors';
 
 export const theme = createTheme({
- palette: {
+  palette: {
     primary: {
       main: purple[500],
     },
